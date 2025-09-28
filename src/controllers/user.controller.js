41c8b7f71b28b43ef5abd9a35ebee912710a1a87
ashshiv123 <shivprasadharde95@gmail.com>
2025-09-28 11:54:27 +0530

    dcvb

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import {asyncHandler} from '../utils/asyncHandler.js';
 const registerUser = asyncHandler(async (req, res) => {
     // Registration logic here
     res.status(200).json({
-        message: "hey buddy"
+        message: "hey buddyadd me"
     })
 })
 
